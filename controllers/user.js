e65c51e6f2e7350242879f14182f9bc4daf64df2
moawiah <muawiyah454@gmail.com>
2025-07-23 18:46:24 +0300

    auth users

--- controllers/user.js
+++ controllers/user.js
@@ -4,43 +4,40 @@ const registration = async (req, res) => {
   console.log("registration");
 
   const { username, email, password } = req.body;
+  console.log(username, email, password);
 
   const query = `
     INSERT INTO users (username, email, password)
-    VALUES ($1, $2, $3)
+    VALUES ($1, $2, $3) RETURNING *
   `;
-  const pas = bcrypt.hashSync(password.toLowerCase(), 5);
+  const pas = await bcrypt.hashSync(password.toLowerCase(), 5);
 
   const data = [username.toLowerCase(), email.toLowerCase(), pas];
 
   client
     .query(query, data)
-    .then(() => {
-      res
-        .status(201)
-        .json({ status: true, message: "User registered successfully" });
-          console.log("registration successfully");
-
+    .then((result) => {
+      res.status(201).json({
+        status: true,
+        message: "User registered successfully",
+        data: result.rows[0],
+      });
     })
     .catch((err) => {
-      if (err.code === "23505") {
+      if (err.code == "23505") {
         res
           .status(400)
           .json({ status: false, message: "Email already in use" });
-          
       } else {
-        res
-          .status(500)
-          .json({ status: false, message: "Internal server error" });
+        res.status(500).json({ status: false, message: "Registration failed" });
       }
     });
 };
 
 const login = (req, res) => {
   const { email, password } = req.body;
-  const query = "SELECT email,password FROM users WHERE email = $1";
+  const query = "SELECT email,password,username FROM users WHERE email = $1";
   const data = [email.toLowerCase()];
-  console.log(email, password);
 
   client
     .query(query, data)
@@ -53,7 +50,7 @@ const login = (req, res) => {
       const hashedPassword = result.rows[0].password;
       const isMatch = await bcrypt.compare(password, hashedPassword);
       if (isMatch) {
-        res.status(200).json({ status: true });
+        res.status(200).json({ status: true, data: result.rows[0] });
       } else {
         res
           .status(401)
