3b88000bd554acee33c09289d67c86bf6843ecdd
moawiah <muawiyah454@gmail.com>
2025-07-27 18:38:54 +0300

    get product by id

--- controllers/product.js
+++ controllers/product.js
@@ -1,34 +1,38 @@
 const client = require("../models/db");
 
 const addProduct = (req, res) => {
-  console.log('addProduct');
-  
-  const { product_name, price_product, category_id } = req.body;
+  const { product_name, price_product, information_product, category_id } =
+    req.body;
   const imageFile = req.file;
 
   const imageBuffer = imageFile.buffer;
 
   const query = `
-    INSERT INTO products (product_name, product_image, price_product, category_id)
-    VALUES ($1, $2, $3, $4)
+    INSERT INTO products (product_name, product_image, price_product, information_product ,category_id)
+    VALUES ($1, $2, $3, $4, $5)
     RETURNING *;
   `;
-  const data = [product_name, imageBuffer, price_product, category_id];
+  const data = [
+    product_name,
+    imageBuffer,
+    price_product,
+    information_product,
+    category_id,
+  ];
 
   client
     .query(query, data)
     .then((result) => {
-      res
-        .status(201)
-        .json({ message: "Product added", data: result.rows });
+      res.status(201).json({ message: "Product added", data: result.rows });
     })
     .catch((err) => {
       console.error(err);
       res.status(500).json({ message: "Error adding product" });
     });
 };
-const getProduct = (req, res) => {
-  const query = "SELECT product_name,product_image,price_product from products";
+const getAllProduct = (req, res) => {
+  const query =
+    "SELECT id,product_name,product_image,price_product,information_product from products";
   client
     .query(query)
     .then((result) => {
@@ -47,4 +51,24 @@ const getProduct = (req, res) => {
       res.status(500).json({ message: "Failed to fetch products" });
     });
 };
-module.exports = { addProduct, getProduct };
+const getProductById = (req, res) => {
+  const id = req.params.id;
+  const query = "SELECT * FROM products WHERE id =$1";
+  client
+    .query(query, [id])
+    .then((result) => {
+      const products = result.rows.map(function (elements) {
+        return {
+          ...elements,
+          product_image: elements.product_image.toString("base64"),
+        };
+      });
+      res.status(200).json({
+        data: products,
+      });
+    })
+    .catch(() => {
+      res.status(500).json({ message: "Failed to fetch product" });
+    });
+};
+module.exports = { addProduct, getAllProduct, getProductById };
--- routes/product.js
+++ routes/product.js
@@ -2,9 +2,14 @@ const express = require("express");
 const multer = require("multer");
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
-const { addProduct, getProduct } = require("../controllers/product");
+const {
+  addProduct,
+  getAllProduct,
+  getProductById,
+} = require("../controllers/product");
 
 const productRoute = express.Router();
 productRoute.post("/add", upload.single("product_image"), addProduct);
-productRoute.get("/", getProduct);
+productRoute.get("/", getAllProduct);
+productRoute.get("/:id", getProductById);
 module.exports = productRoute;
